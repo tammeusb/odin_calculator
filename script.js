b7d78a530c9615c2d35e6fe979f8c1124135c8a4
Brock <tammeusb@gmail.com>
2024-05-13 10:32:28 -0400

    Create operate() and four operation functions

---
+++ script.js
@@ -0,0 +1,28 @@
+const add = function(x,y) {
+    return x + y
+}
+
+const subtract = function(x,y) {
+    return x - y
+}
+
+const multiply = function(x,y) {
+    return x * y
+}
+
+const divide = function(x,y) {
+    return x / y
+}
+
+//variables for display
+
+let firstNumber = 0;
+let secondNumber = 0;
+let operator = 0;
+
+const operate = function(x,y,operator) {
+    if (operator === '+') return add(x,y)
+    if (operator === '-') return subtract(x,y)
+    if (operator === '*') return multiply(x,y)
+    if (operator === '/') return divide(x,y)
+}
